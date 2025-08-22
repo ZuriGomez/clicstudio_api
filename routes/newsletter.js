8bfbfd53bcb54bb2c63b3acffa32583e82d77e42
ZuriGomez <zgo522@gmail.com>
2025-08-22 15:06:51 -0400

    Comment out the test route from newsletter

--- routes/newsletter.js
+++ routes/newsletter.js
@@ -5,9 +5,9 @@ const { addSubscriber } = require('../controllers/newsletterController');
 // POST - Add subscriber
 router.post('/', addSubscriber);
 
-// GET - Test route
-router.get('/', (req, res) => {
-  res.send('Newsletter API is working 🚀');
-});
+// // GET - Test route
+// router.get('/', (req, res) => {
+//   res.send('Newsletter API is working 🚀');
+// });
 
 module.exports = router;
