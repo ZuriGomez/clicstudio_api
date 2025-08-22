aff789f9f0f103ed2c86ebfdc0be55f147b45a80
ZuriGomez <zgo522@gmail.com>
2025-08-22 15:01:20 -0400

    change newsletter routes

--- routes/newsletter.js
+++ routes/newsletter.js
@@ -1,9 +1,13 @@
-// routes/newsletter.js
 const express = require('express');
 const router = express.Router();
 const { addSubscriber } = require('../controllers/newsletterController');
 
-// POST /api/newsletter → Add a new subscriber
+// POST - Add subscriber
 router.post('/', addSubscriber);
+
+// GET - Test route
+router.get('/', (req, res) => {
+  res.send('Newsletter API is working 🚀');
+});
 
 module.exports = router;
